43dcaa6fd42e2537c66ad1834df31e71a8cb86bb
gnomishperson <george@corephp.com>
2013-03-22 14:51:08 -0400

    Initial commit of repo. Includes symlink build necessities copied from jomdefender.

---
+++ src/mailchimp.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package   Chimp Your Joomla!
+ * @copyright (C) 2001 by 'corePHP' - All rights reserved!
+ * @license   GNU/GPL2
+ */
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport('joomla.plugin.plugin');
+
+class plgUserMailchimp extends JPlugin
+{
+
+	function plgUserMailchimp(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+	}
+
+	function onUserAfterSave($user, $isnew, $success, $msg)
+	{
+		global $mainframe;
+
+
+        if($user) {
+    		// Lets pull the API layer
+    		require_once(JPATH_PLUGINS.'/user/mailchimp/MCAPI.class.php');
+    
+    		// Lets grab all the required params now
+    		$mc_username     = $this->params->get('mc_username');
+            $mc_listid       = $this->params->get('mc_listid');
+    		$mc_autoregister = $this->params->get('mc_autoregister', 1);
+    
+    		// Lets put in our API key and set the $api value
+    		$api = new MCAPI($mc_username);
+    
+    		// Create a First and Last Name and place it in the $mergeVars
+    		$name = explode( ' ', $user['name'] );
+    
+    		$mergeVars = array(	'FNAME'=>$name[0],
+    							'LNAME'=>$name[1]);
+    
+    		if( $mc_autoregister == 1 ) {
+    			$mc_autoregister = true;
+    		} else {
+    			$mc_autoregister = false;
+    		}
+    
+    		if ($isnew)
+    		{
+    			if( $api->listSubscribe( $mc_listid,
+    				$user['email'], $mergeVars, 'html',
+    				$mc_autoregister ) === true
+    			) {
+    				// You have just launched a nuclear warhead - what a success
+    			}else{
+    				// Ekk - epic failure
+    				//echo 'Error code: '.$api->errorCode.'. Error message: '.$api->errorMessage;die();
+    			}
+    		}
+    		else
+    		{
+    			//This user is not new - do nothing
+    		}
+        }
+	}
+}
