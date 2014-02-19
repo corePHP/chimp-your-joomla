1d3adceb1374a8fb9cfb7956b4d5056fbbca2bed
Steven Pignataro <steven@corephp.com>
2014-02-19 13:57:31 -0500

    Updated Chimp Your Joomla! to be a basic package
    Signed-off-by: Steven Pignataro <steven@corephp.com>

--- src/plugins/user/chimpyourjoomla/chimpyourjoomla.php
+++ src/plugins/user/chimpyourjoomla/chimpyourjoomla.php
@@ -1,53 +1,50 @@
 <?php
 /**
  * @package   Chimp Your Joomla!
- * @copyright (C) 2011 by 'corePHP' - All rights reserved!
+ * @copyright (C) 2011-2014 by 'corePHP' - All rights reserved!
  * @license   GNU/GPL2
  */
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted index access' );
 
 jimport('joomla.plugin.plugin');
 
-class plgUserChimpyourjoomla extends JPlugin
+class plgSystemChimpYourJoomla extends JPlugin
 {
-
-	function plgChimpyourjoomla(& $subject, $config)
+	function onUserAfterSave( $user, $isnew, $success, $msg )
 	{
-		parent::__construct($subject, $config);
-	}
-
-	function onUserAfterSave($user, $isnew, $success, $msg)
-	{
-		
-		if($user) {
+		if( $user ) {
 			// Lets pull the API layer
-			require_once(JPATH_PLUGINS.'/user/chimpyourjoomla/includes/MCAPI.class.php');
+			require_once( JPATH_PLUGINS . '/user/chimpyourjoomla/includes/mailchimp.php' );
 
 			// Lets grab all the required params now
-			$mc_username     = $this->params->get('mc_username');
-			$mc_listid       = $this->params->get('mc_listid');
-			$mc_autoregister = $this->params->get('mc_autoregister', 1);
+			$chimp_api       = $this->params->get( 'chimp_api' );
+			$chimp_list      = $this->params->get( 'chimp_list' );
+			$chimp_auto      = $this->params->get( 'chimp_auto', 0 );
+
+			// Just a quick check
+			if( !$chimp_api || !$chimp_list ) return;
 
 			// Lets put in our API key and set the $api value
-			$api = new MCAPI($mc_username);
+			$mc = new MailChimp( $chimp_api );
 
 			// Create a First and Last Name and place it in the $mergeVars
 			$name = explode( ' ', $user['name'] );
 
 			$mergeVars = array(	'FNAME'=>$name[0],
-								'LNAME'=>$name[1]);
+								'LNAME'=>$name[1]
+								);
 
-			if( $mc_autoregister == 1 ) {
-				$mc_autoregister = true;
+			if( $chimp_auto == 1 ) {
+				$chimp_auto = true;
 			} else {
-				$mc_autoregister = false;
+				$chimp_auto = false;
 			}
 
-			if ($isnew) {
-				$api->listSubscribe( $mc_listid,
-					$user['email'], $mergeVars, 'html',
-					$mc_autoregister );
+			if ( $isnew ) {
+				mc::add( $mc, $chimp_list, $user['email'], $mergeVars, $chimp_auto );
+			} else {
+				mc::update( $mc, $chimp_list, $user['email'], $mergeVars );
 			}
 		}
 	}
